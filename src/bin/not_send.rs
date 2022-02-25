b72eece9541d35e558f2d35b343d22878adcd214
Jack O'Connor <oconnor663@gmail.com>
2022-02-25 13:19:36 -0500

    simplify the not_send example

--- src/bin/not_send.rs
+++ src/bin/not_send.rs
@@ -1,14 +1,12 @@
 use std::rc::Rc;
-use std::sync::{Arc, Mutex};
+use std::sync::Mutex;
 use std::thread;
 use std::time::Duration;
 
 fn main() {
-    let output = Arc::new(Mutex::new(Vec::new()));
+    let output = Rc::new(Mutex::new(Vec::new()));
     let output_clone = output.clone();
-    let this_is_not_send = Rc::new(42);
     thread::spawn(|| {
-        drop(this_is_not_send);
         background_count(output_clone);
     });
     loop {
@@ -17,7 +15,7 @@ fn main() {
     }
 }
 
-fn background_count(output: Arc<Mutex<Vec<u64>>>) {
+fn background_count(output: Rc<Mutex<Vec<u64>>>) {
     for i in 0.. {
         thread::sleep(Duration::from_secs(1));
         output.lock().unwrap().push(i);
